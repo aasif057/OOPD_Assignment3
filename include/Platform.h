ea5fa0b650ce3a52d054757a7e8c0cdbe1d81642
aasif057 <mdaasifxii@gmail.com>
2024-11-15 19:54:38 +0530

    Platform header and class file updated

--- include/Platform.h
+++ include/Platform.h
@@ -7,9 +7,6 @@ public:
     // Pure virtual function to get stoppage interval
     virtual int getStoppageInterval() const = 0;
     
-    // Pure virtual function to set stoppage interval
-    virtual void setStoppageInterval(int interval) = 0;
-    
     // Virtual destructor
     virtual ~Platform() {}
 };
@@ -17,17 +14,15 @@ public:
 // Derived StoppagePlatform class (can accommodate stoppages)
 class StoppagePlatform : public Platform {
 public:
-    // Constructor taking stoppage interval as argument
-    StoppagePlatform(int interval);
+    StoppagePlatform();  // Default constructor
+    int getStoppageInterval() const override;  // 30 minutes stoppage interval
+};
 
-    // Overridden function to get stoppage interval
-    int getStoppageInterval() const override;
-
-    // Overridden function to set stoppage interval
-    void setStoppageInterval(int interval) override;
-
-private:
-    int stoppageInterval;  // Stores the stoppage interval
+// Derived ThroughPlatform class (can accommodate through trains)
+class ThroughPlatform : public Platform {
+public:
+    ThroughPlatform();  // Default constructor
+    int getStoppageInterval() const override;  // 10 minutes through train interval
 };
 
 #endif
--- src/Platform.cpp
+++ src/Platform.cpp
@@ -1,14 +1,17 @@
 #include "Platform.h"
 
-// StoppagePlatform constructor, initializes stoppage interval
-StoppagePlatform::StoppagePlatform(int interval) : stoppageInterval(interval) {}
+// Default constructor for StoppagePlatform, sets interval to 30 minutes
+StoppagePlatform::StoppagePlatform() {}
 
-// Get the stoppage interval
+// Get the stoppage interval for StoppagePlatform (every 30 minutes)
 int StoppagePlatform::getStoppageInterval() const {
-    return stoppageInterval;
+    return 30;  // 30-minute stoppage interval
 }
 
-// Set the stoppage interval
-void StoppagePlatform::setStoppageInterval(int interval) {
-    stoppageInterval = interval;
+// Default constructor for ThroughPlatform, sets interval to 10 minutes
+ThroughPlatform::ThroughPlatform() {}
+
+// Get the stoppage interval for ThroughPlatform (every 10 minutes)
+int ThroughPlatform::getStoppageInterval() const {
+    return 10;  // 10-minute through train interval
 }
